9ee2822c2a828402c204a9019b361ce16b44a3e6
ukihsoroy <ko.shen@hotmail.com>
2020-08-14 11:14:59 +0800

    #naming of zk

---
+++ client-sdk/src/main/java/org/ko/microservice/namingofzk/NamingOfZkApplication.java
@@ -0,0 +1,13 @@
+package org.ko.microservice.namingofzk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamingOfZkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamingOfZkApplication.class, args);
+	}
+
+}
---
+++ server1/src/test/java/org/ko/microservice/namingofzk/NamingOfZkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ko.microservice.namingofzk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingOfZkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
