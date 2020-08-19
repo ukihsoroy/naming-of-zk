878b2a36d83db095748546665ed0c433a2f5bfb9
ukihsoroy <ko.shen@hotmail.com>
2020-08-19 09:01:40 +0800

    #zk

--- client2/src/main/java/org/ko/namingofzk/Client2Application.java
+++ client2/src/main/java/org/ko/namingofzk/Client2Application.java
@@ -1,16 +1,38 @@
 package org.ko.namingofzk;
 
 import org.ko.namingofzk.sdk.client.properties.ZkProperties;
+import org.ko.namingofzk.sdk.client.zk.ServerAddressHolder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableConfigurationProperties(ZkProperties.class)
+@RestController
 public class Client2Application {
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Autowired private ServerAddressHolder serverAddressHolder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Client2Application.class, args);
+	}
+
+	@GetMapping("hello")
+	public String hello() {
+		return restTemplate.getForObject(serverAddressHolder.getUrl(), String.class);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
